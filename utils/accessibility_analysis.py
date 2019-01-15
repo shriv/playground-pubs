2e569350e4eba1d9040813265080cab6a3075b52
Shrividya Ravi <pentheseleia+bitbucket@gmail.com>
2019-01-16 09:08:13 +1300

    Minor changes to get new package versions and Python 3 working.

--- utils/accessibility_analysis.py
+++ utils/accessibility_analysis.py
@@ -16,30 +16,31 @@ from pandana.loaders import osm
 def get_pandana_network(osm_bbox, impedance=5000):
     """
     Utility function to get pandana nodes within analysis bounding box
-     - Function also filters out poorly connected nodes. Still uncertain 
+     - Function also filters out poorly connected nodes. Still uncertain
     if this is required. And if it is, can we find a better set of values?
     - Pandana network is saved to disk to avoid re-download
-    
+
     Args:
-     osm_bbox: OSM-spec'd bounding box as list  
+     osm_bbox: OSM-spec'd bounding box as list
      impedance: used for filtering poorly connected nodes
     Returns:
      network: pandana network
     """
-    
+
     # Define some parameters
     bbox_string = '_'.join([str(x) for x in osm_bbox])
     net_filename = 'data/network_{}.h5'.format(bbox_string)
+    print(net_filename)
 
     if os.path.isfile(net_filename):
         # if a street network file already exists, just load the dataset from that
         network = pandana.network.Network.from_hdf5(net_filename)
     else:
         # otherwise, query the OSM API for the street network within the specified bounding box
-        network = osm.pdna_network_from_bbox(osm_bbox[0],
+        network = pandana.loaders.osm.pdna_network_from_bbox(osm_bbox[0],
                                              osm_bbox[1],
                                              osm_bbox[2],
-                                             osm_bbox[3], 
+                                             osm_bbox[3],
                                              network_type='walk')
 
 
@@ -55,17 +56,17 @@ def get_pandana_network(osm_bbox, impedance=5000):
 def get_accessibility(network, pois_df, distance=5000, num_pois=10):
     """
     Calculate accesibility metric per node in pandana network
-    
+
     Args:
      network: pandana network
-     pois_df: dataframe of POIS. Fuel stations here. 
-     distance: Limit of accessibility analysis. 
+     pois_df: dataframe of POIS. Fuel stations here.
+     distance: Limit of accessibility analysis.
      num_pois: integer to calculate nth closest POIS
-    
+
     Returns:
-     accessibility: dataframe. Size is #nodes rows, num_pois columns 
+     accessibility: dataframe. Size is #nodes rows, num_pois columns
     """
-    
+
     network.precompute(distance + 1)
     network.set_pois(category='all',
                      x_col=pois_df['lon'],
@@ -88,19 +89,19 @@ def plot_accessibility(network, accessibility,
 
     title = 'Walking distance (m) to nearest {} around {}'.format(amenity_type,
                                                                   place_name)
- 
+
     # network aggregation plots are the same as regular scatter plots,
     # but without a reversed colormap
     # Make the cmap management more generic?
     agg_plot_kwargs = plot_kwargs.copy()
     agg_plot_kwargs['cmap'] = plot_kwargs['cmap'].strip('_r')
-    
+
     # Plot
-    bmap, fig, ax = network.plot(accessibility, 
-                                 bbox=osm_bbox, 
-                                 plot_kwargs=plot_kwargs, 
-                                 fig_kwargs=fig_kwargs, 
-                                 bmap_kwargs=bmap_kwargs, 
+    bmap, fig, ax = network.plot(accessibility,
+                                 bbox=osm_bbox,
+                                 plot_kwargs=plot_kwargs,
+                                 fig_kwargs=fig_kwargs,
+                                 bmap_kwargs=bmap_kwargs,
                                  cbar_kwargs=cbar_kwargs)
     ax.set_title(title,  fontsize=15)
     ax.set_facecolor('w')
