d3fd5595589636f4ff1e014cb626728f84102694
Shrividya Ravi <pentheseleia+bitbucket@gmail.com>
2019-01-16 08:56:50 +1300

    Minor Python 3 changes.

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -14,10 +14,10 @@ def convert_list_string(list_string):
     """
     Converts a string list to a list of strings
     u'[a, b, b]' --> ['a', 'b', 'c']
-    Needed to expand the nodes columns in ways 
+    Needed to expand the nodes columns in ways
     """
-    list_unicode_string =  unicode(list_string, "utf-8")
-    return ast.literal_eval(list_unicode_string)
+    # list_unicode_string =  list_string.decode("utf-8")
+    return ast.literal_eval(str(list_string))
 
 
 ##############
@@ -29,15 +29,15 @@ def generate_overpass_query(tags, objects,
                             entities=["amenity"]):
     """
     Generate and return Overpass query string
-    Permuation of entities, tags and objects. 
+    Permuation of entities, tags and objects.
     So, 2 objects, 3 tags and 2 entities will generate 12 sub-queries
-    
+
     Args:
      tags: list of tags (e.g. 'fuel')
      objects: list of objects (e.g. nodes, ways)
      osm_bbox: vertex list of OSM bounding box convention. Order is: (S, W, N, E)
      entities: list of entities (amenity by default)
-    
+
     Returns:
      compactOverpassQLstring: query string
     """
@@ -51,10 +51,10 @@ def generate_overpass_query(tags, objects,
                                                                             osm_bbox[1],
                                                                             osm_bbox[2],
                                                                             osm_bbox[3])
-    compactOverpassQLstring += ');out body;>;out skel qt;'    
+    compactOverpassQLstring += ');out body;>;out skel qt;'
     return compactOverpassQLstring
 
-    
+
 def get_osm_data(compactOverpassQLstring, osm_bbox):
     """
     Get Data from OSM via Overpass. Convert JSON to Pandas dataframe. Save.
@@ -62,10 +62,10 @@ def get_osm_data(compactOverpassQLstring, osm_bbox):
 
     Args:
      compactOverpassQLstring: Query string
-     osm_bbox: OSM-spec'd bounding box as list  
+     osm_bbox: OSM-spec'd bounding box as list
 
     Returns:
-     osmdf: pandas dataframe of extracted JSON 
+     osmdf: pandas dataframe of extracted JSON
     """
 
     # Filename
@@ -84,20 +84,18 @@ def get_osm_data(compactOverpassQLstring, osm_bbox):
         osm = requests.get(osmurl, params=osmrequest)
 
         # Convert the results to JSON and get the requested data from the 'elements' key
-        # The other keys in osm.json() are metadata guff like 'generator', 'version' of API etc. 
+        # The other keys in osm.json() are metadata guff like 'generator', 'version' of API etc.
         osmdata = osm.json()
         osmdata = osmdata['elements']
         # Convert JSON output to pandas dataframe
         for dct in osmdata:
-            if dct.has_key('tags'):
-                for key, val in dct['tags'].iteritems():
+            if 'tags' in dct.keys():
+                for key, val in dct['tags'].items():
                     dct[key] = val
                 del dct['tags']
             else:
                 pass
         osm_df = pd.DataFrame(osmdata)
-        # osm_df.to_csv(osm_filename, header=True, index=False, encoding='utf-8')
-        
     return osm_df
 
 
@@ -109,33 +107,33 @@ def get_osm_data(compactOverpassQLstring, osm_bbox):
 def extend_ways_to_node_view(osmdf):
     """
     """
-    
+
     osmdf_ways = osmdf.query('type == "way"')[['id', 'nodes', 'type']]
     osmdf_nodes = osmdf.query('type == "node"')[['id', 'lat', 'lon']]
-    osmdf_ways['nodes'] = osmdf_ways['nodes'].astype(str)
+
+    # Expanding way node list to one row per node. So many
     osmdf_ways['nodes'] = osmdf_ways['nodes'].apply(convert_list_string)
-    
     osmdf_ways_clean = (osmdf_ways
                         .set_index(['id', 'type'])['nodes']
                         .apply(pd.Series)
                         .stack()
                         .reset_index())
-
     osmdf_ways_clean.columns = ['way_id', 'type', 'sample_num', 'nodes']
-    # osmdf_nodes['id'] = osmdf_nodes['id'].astype(str)
-    # osmdf_ways_clean['nodes'] = osmdf_ways_clean['nodes'].astype(str) 
-    
+
+    # Merge cleaned ways with nodes with one row per node.
+    # Way df is now has many rows per way_id
     osmdf_clean = pd.merge(osmdf_ways_clean,
                            osmdf_nodes,
                            left_on='nodes',
                            right_on='id').drop(['nodes'], axis=1)
+
     return osmdf_clean
-    
+
 
 def coords_df_to_geopandas_points(osmdf, crs={'init': u'epsg:4167'}):
     """
     """
-    
+
     osmdf['Coordinates'] = list(zip(osmdf.lon, osmdf.lat))
     osmdf['Coordinates'] = osmdf['Coordinates'].apply(Point)
     points_osmdf_clean = geopandas.GeoDataFrame(osmdf, geometry='Coordinates', crs=crs)
@@ -145,7 +143,7 @@ def coords_df_to_geopandas_points(osmdf, crs={'init': u'epsg:4167'}):
 def geopandas_points_to_poly(points_df, crs={'init': u'epsg:4167'}):
     """
     """
-    
+
     points_df['geometry'] = points_df['Coordinates'].apply(lambda x: x.coords[0])
     poly_osmdf_clean = (points_df
                         .groupby('way_id')['geometry']
@@ -172,4 +170,3 @@ def geopandas_to_lat_lon(gpd_df, column):
     x,y = [list(t) for t in zip(*map(getXY, gpd_df[column]))]
     df = pd.DataFrame({'lon': x, 'lat': y})
     return df
-
